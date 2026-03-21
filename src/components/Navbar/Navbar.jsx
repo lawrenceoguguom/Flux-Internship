230e038e7ac1791c17577672fe29c302397a7be0
Oguguom Lawrence <lawrencecally3@gmail.com>
2026-03-21 16:25:22 +0100

    Revamped the text color of the logo, footer and the hero sections

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -16,10 +16,10 @@ const Navbar = () => {
          </div>
 
             <ul className={menuOpen ? "nav-links open" : "nav-links"}>
-                <li><a href="#hero" className='active'>Home</a></li>
+                <li><a href="#hero" className='active' onClick={closeMenu}>Home</a></li>
                 {/* <li><a href="#about">About</a></li> */}
-                <li><a href="#services">Services</a></li>
-                <li><a href="#contact">Work</a></li>
+                <li><a href="#services" onClick={closeMenu}>Services</a></li>
+                <li><a href="#contact" onClick={closeMenu}>Work</a></li>
                 {/* <li><a href="#process">Our Process</a></li> */}
                 {/* <li><a href="#price">Pricing</a></li> */}
             </ul>
